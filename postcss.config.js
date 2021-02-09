35d39c601f2f3cd4384421401f4b9558d8fd713f
kyledwagner <kylewag@me.com>
2021-02-09 18:14:17 +0700

    Updating folder structure.

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,7 @@
 module.exports = {
     plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-        process.env.NODE_ENV === 'production' && require('@fullhuman/postcss-purgecss')({
-            content: [
-                './public/index.html',
-            ],
-            defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-        })
+      require('postcss-import'),
+      require('tailwindcss'),
+      require('autoprefixer'),
     ]
-}
+  }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: [],
+  purge: [
+    './public/index.html',
+    './public/src/css/main.css',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
